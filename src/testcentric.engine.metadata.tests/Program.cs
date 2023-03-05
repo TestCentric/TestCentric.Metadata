d07ec56c98e9f25d13b898222a4375b1169f74a3
Charlie Poole <charliepoole@gmail.com>
2023-03-05 02:25:20 -0800

    Add tests

---
+++ src/testcentric.engine.metadata.tests/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using NUnitLite;
+
+namespace TestCentric.Metadata
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+#if NETFRAMEWORK
+            return new AutoRun().Execute(args);
+#else
+            return new TextRunner(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+#endif
+        }
+    }
+}
